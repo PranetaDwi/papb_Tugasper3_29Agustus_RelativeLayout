b61740dba2f52a36c49e6d79bc7397901e9d8c9e
PranetaDwi <pranetadwiindarti2004@mail.ugm.ac.id>
2023-09-03 05:50:27 +0700

    Initial commit

---
+++ app/src/main/java/com/neta/tugasper3_29agustus_relativelayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.neta.tugasper3_29agustus_relativelayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
